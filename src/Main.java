1e909ba8c4de892d5e971638989aa19082fc11af
Soprodevento <Sashinka2803@gmail.com>
2024-03-18 23:39:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Was it a cat I saw?"));
+    }
+    public static boolean isPalindrome(String text) {
+        text = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        return text.equals(new StringBuffer().append(text).reverse().toString());
+    }
+
+}
